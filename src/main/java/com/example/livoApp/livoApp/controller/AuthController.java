762c86d9ee084641e7c6b98195e764b591601865
AnishMorde <anishmorde@gmail.com>
2025-08-17 16:03:39 +0530

    add health check endpoint and enhance ApiResponse class

--- src/main/java/com/example/livoApp/livoApp/controller/AuthController.java
+++ src/main/java/com/example/livoApp/livoApp/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.livoApp.livoApp.controller;
 
 
+import com.example.livoApp.livoApp.advices.ApiResponse;
 import com.example.livoApp.livoApp.dto.LoginDto;
 import com.example.livoApp.livoApp.dto.LoginResponseDto;
 import com.example.livoApp.livoApp.dto.SignUpRequestDto;
@@ -13,10 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
@@ -30,6 +28,12 @@ public class AuthController {
     public ResponseEntity<UserDto> signUp(@RequestBody SignUpRequestDto sign){
         return new ResponseEntity<>(authService.signUp(sign) , HttpStatus.CREATED);
     }
+
+    @GetMapping("/health")
+    public ApiResponse health() {
+        return new ApiResponse("health is ok", "SUCCESS");
+    }
+
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto , HttpServletRequest request, HttpServletResponse response){
